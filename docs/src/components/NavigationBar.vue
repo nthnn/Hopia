c9b77501e6ea2a19a9ad92030a5560e88b9ab836
nthnn <nathanneisip@gmail.com>
2023-09-22 01:20:26 +0800

    Fixed NavigationBar.vue bugs.

--- docs/src/components/NavigationBar.vue
+++ docs/src/components/NavigationBar.vue
@@ -3,8 +3,8 @@ import { computed, reactive } from "vue";
 import { RouteLocationNormalizedLoaded, RouterLink, useRoute } from "vue-router";
 
 const route: RouteLocationNormalizedLoaded = useRoute();
-const pageData = reactive({
-    currentRoute: computed(()=> route.name.toString())
+var pageData = reactive({
+    currentRoute: computed(()=> route.name)
 });
 </script>
 
@@ -12,24 +12,24 @@ const pageData = reactive({
     <nav class="navbar navbar-expand-lg bg-light shadow-sm fixed-top" data-bs-theme="light">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">Hopia</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-main" aria-controls="navbar-main" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
-            <div class="collapse navbar-collapse" id="navbarColor01">
+            <div class="collapse navbar-collapse" id="navbar-main">
                 <ul class="navbar-nav me-auto">
                     <li class="nav-item">
-                        <RouterLink to="/" :class="pageData.currentRoute.toString() == 'home' ? 'active nav-link' : 'nav-link'">Home</RouterLink>
+                        <RouterLink to="/" v-bind:class="pageData.currentRoute == 'home' ? 'active nav-link' : 'nav-link'">Home</RouterLink>
                     </li>
                     <li class="nav-item">
-                        <RouterLink to="/docs" :class="pageData.currentRoute.toString() == 'docs' ? 'active nav-link' : 'nav-link'">Documentation</RouterLink>
+                        <RouterLink to="/docs" v-bind:class="pageData.currentRoute == 'docs' ? 'active nav-link' : 'nav-link'">Documentation</RouterLink>
                     </li>
                     <li class="nav-item">
-                        <RouterLink to="/about" :class="pageData.currentRoute.toString() == 'about' ? 'active nav-link' : 'nav-link'">About</RouterLink>
+                        <RouterLink to="/about" v-bind:class="pageData.currentRoute == 'about' ? 'active nav-link' : 'nav-link'">About</RouterLink>
                     </li>
                 </ul>
             </div>
         </div>
     </nav>
-    <br/><br/><br/>
+    <br/><br/><br/><br/><br/><br/>
 </template>
--- docs/src/views/HomeView.vue
+++ docs/src/views/HomeView.vue
@@ -1,3 +1,12 @@
 <template>
-  <h1>Hopia</h1>
+    <div class="row">
+        <div class="col-lg-6">
+            <h1>Hopia</h1>
+
+            <p></p>
+        </div>
+
+        <div class="col-lg-6">
+        </div>
+    </div>
 </template>
